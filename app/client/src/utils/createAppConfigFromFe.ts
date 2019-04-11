e01220cd03bb2bfc2bfde96b119a4c078cad75fb
Michal Bureš <michal.bures@travelportgds.cz>
2019-04-11 15:03:30 +0200

    Fix catching error

--- app/client/src/utils/createAppConfigFromFe.ts
+++ app/client/src/utils/createAppConfigFromFe.ts
@@ -9,50 +9,58 @@ import {
 import { AppConfig } from '@client/types/AppConfig'
 import { getUrlParameterValue } from '@client/utils/getUrlParameterValue'
 import { AppError } from '@shared/errors/AppError'
+import { InvalidClientInputError } from '@shared/errors/InvalidClientInputError'
 import { SupportedLanguageEnum } from '@shared/translation/SupportedLanguageEnum'
 
 // tslint:disable-next-line:no-any
 declare var dataLayer: any // global variable from html
 
 export const createAppConfigFromFe = (doc: Document, url: string): AppConfig | undefined => {
-  // tslint:disable-next-line:no-unsafe-any
-  if (!dataLayer || dataLayer.length === 0) {
-    throw new AppError('Empty dataLayer')
+  try {
+    // tslint:disable-next-line:no-unsafe-any
+    if (!dataLayer || dataLayer.length === 0) {
+      throw new AppError('Empty dataLayer')
+    }
+
+    // tslint:disable-next-line:no-unsafe-any
+    if (!dataLayer[0].searchVariables) {
+      throw new AppError('DataLayer not contain searchVariables')
+    }
+
+    const emailToContinueWatching = getUrlParameterValue(url, 'flightWatchdogContinue')
+    const watcherIdToDelete = getUrlParameterValue(url, 'flightWatchdogDelete')
+    const email = getUrlParameterValue(url, 'email')
+    const langElement = document.getElementsByTagName('html').item(0)
+    const lang = new ValidLanguage(
+      langElement && langElement.getAttribute('lang'),
+      Object.values(SupportedLanguageEnum)
+    )
+
+    const lowestPriceElement = doc.querySelector('.offersData .offerContainer .buttons .tariff-btn strong')
+
+    if (!lowestPriceElement) {
+      throw new AppError('Lowest price not found')
+    }
+
+    // tslint:disable:no-unsafe-any
+    return {
+      arrival: dataLayer[0].searchVariables.roundTrip
+        ? new ValidDate(dataLayer[0].searchVariables.returnDate)
+        : undefined,
+      departure: new ValidDate(dataLayer[0].searchVariables.departureDate),
+      destination: new ValidIATALocationList(dataLayer[0].searchVariables.to),
+      emailForWatcherDelete: email ? new ValidEmail(email) : undefined,
+      emailToContinueWatching: emailToContinueWatching ? new ValidEmail(emailToContinueWatching) : undefined,
+      origin: new ValidIATALocationList(dataLayer[0].searchVariables.from),
+      flightType: dataLayer[0].searchVariables.roundTrip ? 'return' : 'oneway',
+      watcherIdToDelete: watcherIdToDelete ? new ValidString(watcherIdToDelete) : undefined,
+      lowestPrice: new ValidPrice(normalizeText(lowestPriceElement.innerHTML)),
+      lang
+    }
+    // tslint:enable
+  } catch (e) {
+    throw new InvalidClientInputError(e.message)
   }
-
-  // tslint:disable-next-line:no-unsafe-any
-  if (!dataLayer[0].searchVariables) {
-    throw new AppError('DataLayer not contain searchVariables')
-  }
-
-  const emailToContinueWatching = getUrlParameterValue(url, 'flightWatchdogContinue')
-  const watcherIdToDelete = getUrlParameterValue(url, 'flightWatchdogDelete')
-  const email = getUrlParameterValue(url, 'email')
-  const langElement = document.getElementsByTagName('html').item(0)
-  const lang = new ValidLanguage(langElement && langElement.getAttribute('lang'), Object.values(SupportedLanguageEnum))
-
-  const lowestPriceElement = doc.querySelector('.offersData .offerContainer .buttons .tariff-btn strong')
-
-  if (!lowestPriceElement) {
-    throw new AppError('DataLayer not contain lowest price')
-  }
-
-  // tslint:disable:no-unsafe-any
-  return {
-    arrival: dataLayer[0].searchVariables.roundTrip
-      ? new ValidDate(dataLayer[0].searchVariables.returnDate)
-      : undefined,
-    departure: new ValidDate(dataLayer[0].searchVariables.departureDate),
-    destination: new ValidIATALocationList(dataLayer[0].searchVariables.to),
-    emailForWatcherDelete: email ? new ValidEmail(email) : undefined,
-    emailToContinueWatching: emailToContinueWatching ? new ValidEmail(emailToContinueWatching) : undefined,
-    origin: new ValidIATALocationList(dataLayer[0].searchVariables.from),
-    flightType: dataLayer[0].searchVariables.roundTrip ? 'return' : 'oneway',
-    watcherIdToDelete: watcherIdToDelete ? new ValidString(watcherIdToDelete) : undefined,
-    lowestPrice: new ValidPrice(normalizeText(lowestPriceElement.innerHTML)),
-    lang
-  }
-  // tslint:enable
 }
 
 const normalizeText = (s: string) => s.replace(/&nbsp;/g, ' ')
