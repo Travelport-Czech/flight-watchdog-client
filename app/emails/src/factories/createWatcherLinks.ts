65d8da94aec2052497e5ccaab44171d6393c707c
Michal Bureš <michal.bures@travelportgds.cz>
2019-04-12 13:13:43 +0200

    Modify emails links

--- app/emails/src/factories/createWatcherLinks.ts
+++ app/emails/src/factories/createWatcherLinks.ts
@@ -10,28 +10,36 @@ const createResultLink = (watcher: WatcherParams): string => {
       throw new AppLogicError('Missing arrival for return flight')
     }
 
-    return `/index.php?action=vFlights\
-&flights[0][departureDate]=${watcher.departure.formatToSystem()}\
-&flights[0][destination]=${watcher.destination.toString()}\
-&flights[0][origin]=${encodeURIComponent(watcher.origin.toString())}\
-&flights[1][departureDate]=${watcher.arrival.formatToSystem()}\
-&flights[1][destination]=${watcher.origin.toString()}\
-&flights[1][origin]=${watcher.destination.toString()}\
-&travelers[0]=ADT\
-&returnTicket=on\
-&dateVariants=exact\
-&step=ChooseFromFour`
+    return `/booking/api/search/v3?\
+id_dealer=10\
+&client_encoding=utf-8\
+&lang=cs\
+&id=airticket\
+&ui_formtype=round_trip\
+&arrival_destination_1_short=${encodeURIComponent(watcher.destination.toString())}\
+&arrival_destination_1=${encodeURIComponent(watcher.destination.toString())}\
+&departure_destination_1_short=${encodeURIComponent(watcher.origin.toString())}\
+&departure_destination_1=${encodeURIComponent(watcher.origin.toString())}\
+&departure_date_1=${watcher.departure.formatToSystem()}\
+&departure_date_2==${watcher.arrival.formatToSystem()}\
+&passengers_adt=1\
+`
   }
 
   if (watcher.flightType === 'oneway') {
-    return `/index.php?action=vFlights\
-&flights[0][departureDate]=${watcher.departure.formatToSystem()}\
-&flights[0][destination]=${watcher.destination.toString()}\
-&flights[0][origin]=${encodeURIComponent(watcher.origin.toString())}\
-&travelers[0]=ADT\
-&returnTicket=\
-&dateVariants=exact\
-&step=ChooseFromFour`
+    return `/booking/api/search/v3?\
+id_dealer=10\
+&client_encoding=utf-8\
+&lang=cs\
+&id=airticket\
+&ui_formtype=oneway\
+&arrival_destination_1_short=${encodeURIComponent(watcher.destination.toString())}\
+&arrival_destination_1=${encodeURIComponent(watcher.destination.toString())}\
+&departure_destination_1_short=${encodeURIComponent(watcher.origin.toString())}\
+&departure_destination_1=${encodeURIComponent(watcher.origin.toString())}\
+&departure_date_1=${watcher.departure.formatToSystem()}\
+&passengers_adt=1\
+`
   }
 
   throw new AppLogicError('Bad flight type')
@@ -39,26 +47,21 @@ const createResultLink = (watcher: WatcherParams): string => {
 
 export const createWatcherLinks = (watcher: WatcherParams, agencyParams: AgencyParams): WatcherLinks => {
   const { dealerId, frontendUrl } = agencyParams
-  const waitPageString = `${frontendUrl.toString()}/index.php?action=vWait&redirect=`
   const dealerIdUrlPart = dealerId ? '&dealer_id=' + dealerId.toString() : ''
   const resultLinkString = frontendUrl.toString() + createResultLink(watcher) + dealerIdUrlPart
 
-  const resultLink = new ValidUrl(waitPageString + encodeURIComponent(resultLinkString) + '&flightWatchdogResult=')
+  const resultLink = new ValidUrl(resultLinkString + '&flightWatchdogResult=')
 
   const continueLink = new ValidUrl(
-    waitPageString +
-      encodeURIComponent(resultLinkString + '&flightWatchdogContinue=' + encodeURIComponent(watcher.email.toString()))
+    resultLinkString + '&flightWatchdogContinue=' + encodeURIComponent(watcher.email.toString())
   )
 
   const deleteLink = new ValidUrl(
-    waitPageString +
-      encodeURIComponent(
-        resultLinkString +
-          '&flightWatchdogDelete=' +
-          encodeURIComponent(watcher.id.toString()) +
-          '&email=' +
-          encodeURIComponent(watcher.email.toString())
-      )
+    resultLinkString +
+      '&flightWatchdogDelete=' +
+      encodeURIComponent(watcher.id.toString()) +
+      '&email=' +
+      encodeURIComponent(watcher.email.toString())
   )
 
   return {
