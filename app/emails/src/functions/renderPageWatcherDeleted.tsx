6f743a30d8b92c41ac190d1907056b02c322018b
Michal Bureš <michal.bures@travelportgds.cz>
2020-01-06 11:59:07 +0100

    Change render page params. BREAKING CHANGE.

--- app/emails/src/functions/renderPageWatcherDeleted.tsx
+++ app/emails/src/functions/renderPageWatcherDeleted.tsx
@@ -1,6 +1,6 @@
 import { PageWatcherDeleted } from '@emails/reactComponents/PageWatcherDeleted'
 import { AgencyParams } from '@emails/types/AgencyParams'
-import { WatcherFullInfo } from '@emails/types/WatcherFullInfo'
+import { WatcherParams } from '@emails/types/WatcherParams'
 import * as React from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
 
@@ -9,8 +9,6 @@ import { renderToStaticMarkup } from 'react-dom/server'
  *
  * Is used by backend server
  */
-export const renderPageWatcherDeleted = (agencyParams: AgencyParams, watcherFullInfo: WatcherFullInfo): string => {
-  return renderToStaticMarkup(
-    <PageWatcherDeleted link={agencyParams.frontendUrl} lang={watcherFullInfo.watcher.lang} />
-  )
+export const renderPageWatcherDeleted = (agencyParams: AgencyParams, watcher: WatcherParams): string => {
+  return renderToStaticMarkup(<PageWatcherDeleted link={agencyParams.frontendUrl} lang={watcher.lang} />)
 }
