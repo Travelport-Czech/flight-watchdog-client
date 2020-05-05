1e9bac125429a58438e9861dc0faf8ae92fa8343
Michal Bureš <michal.bures@travelportgds.cz>
2020-05-05 16:24:37 +0200

    Fix sk translation

--- app/shared/src/translation/translations.ts
+++ app/shared/src/translation/translations.ts
@@ -1,10 +1,12 @@
 import { alTranslation } from '@shared/translation/alTranslation'
 import { csTranslation } from '@shared/translation/csTranslation'
 import { enTranslation } from '@shared/translation/enTranslation'
+import { skTranslation } from '@shared/translation/skTranslation'
 import { SupportedLanguageEnum } from '@shared/translation/SupportedLanguageEnum'
 
 export const translations = {
   [SupportedLanguageEnum.al]: alTranslation,
   [SupportedLanguageEnum.cs]: csTranslation,
-  [SupportedLanguageEnum.en]: enTranslation
+  [SupportedLanguageEnum.en]: enTranslation,
+  [SupportedLanguageEnum.sk]: skTranslation
 }
