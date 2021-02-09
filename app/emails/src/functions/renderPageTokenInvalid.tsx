7912b81252b9b40c2711e38a49d5597c487f3a9c
Michal Bureš <michal.bures@travelportgds.cz>
2021-02-09 15:37:28 +0100

    BREAKING CHANGE. Change lang input type to render functions.

--- app/emails/src/functions/renderPageTokenInvalid.tsx
+++ app/emails/src/functions/renderPageTokenInvalid.tsx
@@ -2,12 +2,17 @@ import { PageTokenInvalid } from '@emails/reactComponents/PageTokenInvalid'
 import * as React from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
 import { SupportedLanguageEnum } from '@shared/translation/SupportedLanguageEnum'
+import { AppLogicError } from '@shared/errors/AppLogicError'
 
 /**
  * Do not delete this function!
  *
  * Is used by backend server
  */
-export const renderPageTokenInvalid = (lang: SupportedLanguageEnum): string => {
-  return renderToStaticMarkup(<PageTokenInvalid lang={lang} />)
+export const renderPageTokenInvalid = (lang: string): string => {
+  const validatedLang = SupportedLanguageEnum[lang]
+  if (!validatedLang) {
+    throw new AppLogicError(`Not supported language ${lang}`)
+  }
+  return renderToStaticMarkup(<PageTokenInvalid lang={validatedLang} />)
 }
--- app/emails/src/functions/renderPageWatcherDeleted.tsx
+++ app/emails/src/functions/renderPageWatcherDeleted.tsx
@@ -3,12 +3,17 @@ import { AgencyParams } from '@emails/types/AgencyParams'
 import * as React from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
 import { SupportedLanguageEnum } from '@shared/translation/SupportedLanguageEnum'
+import { AppLogicError } from '@shared/errors/AppLogicError'
 
 /**
  * Do not delete this function!
  *
  * Is used by backend server
  */
-export const renderPageWatcherDeleted = (agencyParams: AgencyParams, lang: SupportedLanguageEnum): string => {
-  return renderToStaticMarkup(<PageWatcherDeleted link={agencyParams.frontendUrl} lang={lang} />)
+export const renderPageWatcherDeleted = (agencyParams: AgencyParams, lang: string): string => {
+  const validatedLang = SupportedLanguageEnum[lang]
+  if (!validatedLang) {
+    throw new AppLogicError(`Not supported language ${lang}`)
+  }
+  return renderToStaticMarkup(<PageWatcherDeleted link={agencyParams.frontendUrl} lang={validatedLang} />)
 }
