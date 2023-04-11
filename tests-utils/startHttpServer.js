55bf82ae5eea44b9a461de6b9cc5f7a055698cb4
Michal Bureš <michal.ezop@gmail.com>
2023-04-11 11:50:20 +0200

    Remove hapi inert, update deps

--- tests-utils/startHttpServer.js
+++ tests-utils/startHttpServer.js
@@ -10,108 +10,104 @@ const emailClientJsMap = path.join(__dirname, rootPathEmail + '/index.js.map')
 const rootWebDir = path.join(__dirname, './httpPages')
 
 const initServer = async () => {
-  const server = Hapi.server({
-    host: 'localhost',
-    port: 8080,
-    routes: { log: { collect: true } }
-  })
+    const server = Hapi.server({
+        host: 'localhost',
+        port: 8080,
+        routes: { log: { collect: true } },
+    })
 
-  await server.register(require('@hapi/inert'))
+    server.route({
+        method: 'GET',
+        path: '/client/index.js',
+        handler: function (request, h) {
+            return h.file(rootClientJs)
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/client/index.js',
-    handler: function(request, h) {
-      return h.file(rootClientJs)
-    }
-  })
+    server.route({
+        method: 'GET',
+        path: '/client/index.js.map',
+        handler: function (request, h) {
+            return h.file(rootClientJsMap)
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/client/index.js.map',
-    handler: function(request, h) {
-      return h.file(rootClientJsMap)
-    }
-  })
+    server.route({
+        method: 'GET',
+        path: '/client/email.js',
+        handler: function (request, h) {
+            return h.file(emailClientJs)
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/client/email.js',
-    handler: function(request, h) {
-      return h.file(emailClientJs)
-    }
-  })
+    server.route({
+        method: 'GET',
+        path: '/client/email.js.map',
+        handler: function (request, h) {
+            return h.file(emailClientJsMap)
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/client/email.js.map',
-    handler: function(request, h) {
-      return h.file(emailClientJsMap)
-    }
-  })
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, h) {
+            return h.file(rootWebDir + '/index.html')
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, h) {
-      return h.file(rootWebDir + '/index.html')
-    }
-  })
+    server.route({
+        method: 'GET',
+        path: '/lowerPriceEmail',
+        handler: function (request, h) {
+            return h.file(rootWebDir + '/lowerPriceEmail.html')
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/lowerPriceEmail',
-    handler: function(request, h) {
-      return h.file(rootWebDir + '/lowerPriceEmail.html')
-    }
-  })
+    server.route({
+        method: 'GET',
+        path: '/marketingEmail',
+        handler: function (request, h) {
+            return h.file(rootWebDir + '/marketingEmail.html')
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/marketingEmail',
-    handler: function(request, h) {
-      return h.file(rootWebDir + '/marketingEmail.html')
-    }
-  })
+    server.route({
+        method: 'GET',
+        path: '/watcherListEmail',
+        handler: function (request, h) {
+            return h.file(rootWebDir + '/watcherListEmail.html')
+        },
+    })
 
-  server.route({
-    method: 'GET',
-    path: '/watcherListEmail',
-    handler: function(request, h) {
-      return h.file(rootWebDir + '/watcherListEmail.html')
-    }
-  })
-
-  return server
+    return server
 }
 
 let server
 
 const start = async () => {
-  try {
-    console.log('Starting HAPI server...')
-    server = await initServer()
-    console.log('Server starter.')
-    server.events.on('log', (event, tags) => {
-      if (tags.error) {
-        console.log(event);
-      }
-    })
-    server.events.on({ name: 'request', channels: 'app' }, (request, event, tags) => {
-      if (tags.error) {
-        console.log(event);
-      }
-    })
-    await server.start()
-  } catch (err) {
-    console.log('Start server error', err)
-    return
-  }
+    try {
+        console.log('Starting HAPI server...')
+        server = await initServer()
+        console.log('Server starter.')
+        server.events.on('log', (event, tags) => {
+            if (tags.error) {
+                console.log(event)
+            }
+        })
+        server.events.on({ name: 'request', channels: 'app' }, (request, event, tags) => {
+            if (tags.error) {
+                console.log(event)
+            }
+        })
+        await server.start()
+    } catch (err) {
+        console.log('Start server error', err)
+        return
+    }
 }
 const stop = async () => {
-  await server.stop()
+    await server.stop()
 }
 
 start()
-
-
